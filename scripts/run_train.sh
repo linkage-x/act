4e97f61a7492dadd4037dcb6282d27b94350a640
hph <kjustdoitno1@gmail.com>
2025-10-24 09:48:11 +0800

    uu

--- scripts/run_train.sh
+++ scripts/run_train.sh
@@ -3,7 +3,7 @@ cd ~/code/act
 export PYTHONPATH="${PYTHONPATH}:$(pwd)"
 
 # 使用配置文件的简化训练脚本
-CUDA_VISIBLE_DEVICES=6 python3 imitate_episodes.py \
-  --config configs/tasks/fr3_it_1022_101ep.yaml
+CUDA_VISIBLE_DEVICES=7 python3 imitate_episodes.py \
+  --config configs/tasks/fr3_it_1023_61ep.yaml
 
 
--- scripts/run_train2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd ~/code/act
-export PYTHONPATH="${PYTHONPATH}:$(pwd)"
-
-CUDA_VISIBLE_DEVICES=5 python3 imitate_episodes.py \
-  --config configs/tasks/fr3_ip_1022_49ep.yaml
