2fd49f4818cd97651e99a51158538c38022f3fe3
hph <kjustdoitno1@gmail.com>
2025-10-29 09:41:08 +0000

    u

--- scripts/run_train.sh
+++ scripts/run_train.sh
@@ -4,6 +4,6 @@ export PYTHONPATH="${PYTHONPATH}:$(pwd)"
 
 # 使用配置文件的简化训练脚本
 CUDA_VISIBLE_DEVICES=7 python3 imitate_episodes.py \
-  --config configs/tasks/fr3_human_pp_82ep.yaml
+  --config configs/tasks/fr3_mixed_it_1029_pika.yaml
 
 
