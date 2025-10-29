a507d8d0a70907fa80421cc66fec3ffcb94259d4
hph <kjustdoitno1@gmail.com>
2025-10-29 09:44:58 +0000

    fr3_mixed_it_1029_3dmouse

--- scripts/run_train.sh
+++ scripts/run_train.sh
@@ -3,7 +3,7 @@ cd ~/code/act
 export PYTHONPATH="${PYTHONPATH}:$(pwd)"
 
 # 使用配置文件的简化训练脚本
-CUDA_VISIBLE_DEVICES=7 python3 imitate_episodes.py \
-  --config configs/tasks/fr3_mixed_it_1029_pika.yaml
+CUDA_VISIBLE_DEVICES=6 python3 imitate_episodes.py \
+  --config configs/tasks/fr3_mixed_it_1029_3dmouse.yaml
 
 
