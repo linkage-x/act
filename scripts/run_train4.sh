025359f579c08f5c267453ca0d3aca9c96d75f93
hph <kjustdoitno1@gmail.com>
2025-10-30 07:37:06 +0000

    it 4

---
+++ scripts/run_train4.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd ~/code/act
+export PYTHONPATH="${PYTHONPATH}:$(pwd)"
+
+# 使用配置文件的简化训练脚本
+CUDA_VISIBLE_DEVICES=4 python3 imitate_episodes.py \
+  --config configs/tasks/fr3_it_mix_1030_3dmouse_customcfg4.yaml
+
+
