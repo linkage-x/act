6b15d684feaa04cea8135425fc0bc06d8e224f12
hph <kjustdoitno1@gmail.com>
2025-10-30 10:31:19 +0000

    x

--- scripts/run_train2.sh
+++ scripts/run_train2.sh
@@ -3,7 +3,7 @@ cd ~/code/act
 export PYTHONPATH="${PYTHONPATH}:$(pwd)"
 
 # 使用配置文件的简化训练脚本
-CUDA_VISIBLE_DEVICES=5 python3 imitate_episodes.py \
-  --config configs/tasks/fr3_it_mix_1030_3dmouse_customcfg3.yaml
+CUDA_VISIBLE_DEVICES=6 python3 imitate_episodes.py \
+  --config configs/tasks/fr3_it_mix_1030_3dmouse_customcfg5.yaml
 
 
